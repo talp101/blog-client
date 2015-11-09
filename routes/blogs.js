df7de10148dd00c5aec0f1fe557b884936aad619
Tal Peretz <13@1500.co.il>
2015-11-09 22:36:45 +0200

    limiting and orderby the posts

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:id/posts', function(req, res, next) {
-    res.send([{id:1, title:'tal'}, {id:2, title:'reut'}])
+    res.send([{id:1, title:'tal'}, {id:2, title:'reut'}, {id:3, title:'tal'}, {id:4, title:'reut'}])
 });
 
 module.exports = router;
